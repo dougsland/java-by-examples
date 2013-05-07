06a120e41f62e2a9f9fe4d0dd7c231b100d30ad2
Douglas Schilling Landgraf <dougsland@gmail.com>
2013-05-07 10:07:23 -0400

    Adding types

---
+++ types.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2013
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details
+ */
+
+public class types {
+	public static void main(String args[]){
+		int number;
+        boolean easy = true;
+		String str = "myString";
+
+        // Array
+		int[] numList = {1,2,3,4,5};
+        String[] wordsList = {"Linux", "FreeBSD", "OpenBSD", "Windows"};
+
+		double complex = Math.random();
+	}
+}
