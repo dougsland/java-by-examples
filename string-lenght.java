0cf01ba376211ec9ad24471885dde89153ec2049
Douglas Schilling Landgraf <dougsland@gmail.com>
2013-05-29 14:59:47 -0400

    Adding string-lenght.java

---
+++ string-lenght.java
@@ -0,0 +1,18 @@
+/* Copyright (C) 2013
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details
+ */
+
+public class lenght {
+	public static void main(String args[]){
+        String MyString = "my-super-string-length";
+         System.out.println("String: " + MyString);
+         System.out.println("String Length: " + MyString.length());
+	}
+}
